e12e2b12b6a15c3ce6677e9656dff13271084b45
Aaqil456 <72489957+Aaqil456@users.noreply.github.com>
2021-09-10 15:11:49 +0800

    my step up app

--- app/src/main/java/com/example/stepup/ProfileActivity.java
+++ app/src/main/java/com/example/stepup/ProfileActivity.java
@@ -1,8 +1,14 @@
 package com.example.stepup;
 
 import android.content.Intent;
+import android.icu.number.Precision;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +16,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationBarView;
 
-public class ProfileActivity extends AppCompatActivity  {
+import java.text.DecimalFormat;
 
+public class ProfileActivity extends AppCompatActivity  {
+    double ans_bmi=0.00;
+    double height=0,weight=0;
+    EditText heightText,weightText;
+    TextView textViewBmi,textViewStatus;
+    Button btnSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
+        //navbar
         //Initialize and assign var
         BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
         //Set Home Selected
@@ -44,6 +57,89 @@ public class ProfileActivity extends AppCompatActivity  {
                 return false;
             }
         });
+        //navbar code until here
+        btnSet=findViewById(R.id.buttonSet);
+        textViewBmi=findViewById(R.id.TextViewBMICalculate);
+        textViewStatus=findViewById(R.id.TextViewBMIStatus);
+        heightText=findViewById(R.id.heightInput);
+        weightText=findViewById(R.id.weightInput);
+
+
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                height= Double.valueOf(heightText.getText().toString());
+                weight= Double.valueOf(weightText.getText().toString());
+                takeAns(height,weight);
+            }
+        });
+
+
+
+
 
     }
+
+    public void takeAns(double height,double weight){
+        DecimalFormat df = new DecimalFormat("0.00");
+        if(checkWeight()&&checkHeight()){
+            ans_bmi= weight/ Math.pow(height,2);
+            textViewBmi.setText(String.valueOf(df.format(ans_bmi))+"kg/m2");
+            checkStatus(ans_bmi);
+
+        }
+
+    }
+
+    public boolean checkWeight(){
+        String input = weightText.getText().toString();
+        if (input.length() == 0) {
+            Toast.makeText(ProfileActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        double millisInput = Double.parseDouble(input) * 60000;
+        if (millisInput == 0) {
+            Toast.makeText(ProfileActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return  true;
+    }
+
+    public boolean checkHeight(){
+        String input = heightText.getText().toString();
+        if (input.length() == 0) {
+            Toast.makeText(ProfileActivity.this, "Field can't be empty", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        double millisInput = Double.parseDouble(input) * 60000;
+        if (millisInput == 0) {
+            Toast.makeText(ProfileActivity.this, "Please enter a positive number", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return  true;
+    }
+
+    // Prints BMI category
+    public void checkStatus(double bmi) {
+        if(bmi < 18.5) {
+            System.out.println("You are underweight");
+            textViewStatus.setText("underWeight");
+        }else if (bmi < 25) {
+            System.out.println("You are normal");
+            textViewStatus.setText("normal");
+        }else if (bmi < 30) {
+            System.out.println("You are overweight");
+            textViewStatus.setText("overweight");
+
+        }else {
+            System.out.println("You are obese");
+            textViewStatus.setText("Obese");
+        }
+    }
+
+
 }
